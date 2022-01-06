ca3e4e43e3ae1733004770c71660d936d4c3fecd
Wilson Leung <wilsonpsleung@gmail.com>
2022-01-06 22:39:48 +0800

    add responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,8 @@ function App() {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-slate-100">
-      <div className="w-[375px] h-[667px] rounded-lg shadow-lg bg-white flex flex-col p-6 gap-4">
-        <header className="font-bold text-3xl text-center text-red-400 pt-4 pb-4">
+      <div className="sm:w-[375px] sm:h-[667px] w-full h-full rounded-lg shadow-lg bg-white flex flex-col p-6 gap-4">
+        <header className="font-bold text-4xl text-center text-red-400 pt-4 pb-4">
           🍅 Tomato Timer
         </header>
 
