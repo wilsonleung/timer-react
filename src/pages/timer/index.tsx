27e7ec71d7672ad0ccc6692ed071a78bb455f849
Wilson Leung <wilsonpsleung@gmail.com>
2022-01-06 22:17:59 +0800

    add count down effect

--- src/pages/timer/index.tsx
+++ src/pages/timer/index.tsx
@@ -1,24 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../components/button';
 
-const Timer: React.FC = () => {
-  const [counting, setCounting] = useState<boolean>(false);
+const oneSecond = 1000;
 
-  const startCountdownHandler = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): void => {
+function toCountDownString(millSecond: number) {
+  const mins = parseInt(String(millSecond / (60 * oneSecond)));
+  const seconds = (millSecond - mins * 60 * oneSecond) / oneSecond;
+  return `${String(mins).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+const Timer: React.FC = () => {
+  const [counting, setCounting] = useState(false);
+  const [countDown, setCountDown] = useState('25:00');
+
+  const startCountdownHandler = (): void => {
     setCounting(true);
   };
 
-  const stopCountdownHandler = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): void => {
+  const stopCountdownHandler = (): void => {
     setCounting(false);
   };
 
+  useEffect(() => {
+    let cancel: number | undefined = undefined;
+    if (counting) {
+      let tempCount = 25 * 60 * oneSecond;
+      cancel = window.setInterval(() => {
+        tempCount = tempCount - oneSecond;
+        setCountDown(toCountDownString(tempCount));
+      }, oneSecond);
+    } else {
+      if (cancel) {
+        window.clearInterval(cancel);
+      }
+    }
+
+    return () => {
+      if (cancel) {
+        clearInterval(cancel);
+      }
+    };
+  }, [counting]);
+
   return (
     <div className="h-full flex flex-col justify-center items-center gap-8">
-      <h1 className="text-8xl font-bold text-gray-600">25:00</h1>
+      <h1 className="font-mono text-8xl font-bold text-gray-600">
+        {countDown}
+      </h1>
       {counting ? (
         <Button
           text="Stop"
